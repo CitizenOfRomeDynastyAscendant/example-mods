b46215d3967ae74f7d25814e41c2aa8b5c2bc880
CitizenOfRome <rome@rangergo.net>
2021-08-21 11:01:09 +0530

    rename to command_console & bugfix not running default addTrait command

--- mods/command_console/main.js
+++ mods/command_console/main.js
@@ -4,13 +4,13 @@
   checkType: 'general',
   checkAndAct() {
     daapi.addGlobalAction({
-      key: 'command',
+      key: 'command_console',
       action: {
         title: 'Open a daapi Command Console',
-        icon: daapi.requireImage('/command/command.svg'),
+        icon: daapi.requireImage('/command_console/command.svg'),
         isAvailable: true,
         process: {
-          event: '/command/main',
+          event: '/command_console/main',
           method: 'process'
         }
       }
@@ -18,17 +18,18 @@
   },
   methods: {
     process() {
+      daapi.setGlobalFlag({ flag: 'command_console', data: daapi.getGlobalFlag({ flag: 'command_console' }) || 'daapi.addTrait({characterId:daapi.getState().current.id, trait: \'strong\'})' })
       daapi.pushInteractionModalQueue({
         title: 'Command Console',
         message: 'Type a daapi command in the text box below and then tap the run button. Please reach out via the official Discord for help',
-        image: daapi.requireImage('/command/command.svg'),
+        image: daapi.requireImage('/command_console/command.svg'),
         inputs: [
           {
             type: 'text',
             title: 'Command here',
-            value: daapi.getGlobalFlag({ flag: 'command' }) || 'daapi.addTrait({characterId:daapi.getState().current.id, trait: \'strong\'})',
+            value: daapi.getGlobalFlag({ flag: 'command_console' }),
             onChange: {
-              event: '/command/main',
+              event: '/command_console/main',
               method: 'noteCommand'
             }
           }
@@ -37,9 +38,9 @@
           {
             variant: 'info',
             text: 'Run this',
-            icon: daapi.requireImage('/command/command.svg'),
+            icon: daapi.requireImage('/command_console/command.svg'),
             action:{
-              event: '/command/main',
+              event: '/command_console/main',
               method: 'run'
             }
           },
@@ -50,16 +51,16 @@
       })
     },
     noteCommand({input, index}) {
-      daapi.setGlobalFlag({ flag: 'command', data: input.value || '' })
+      daapi.setGlobalFlag({ flag: 'command_console', data: input.value || '' })
     },
     run() {
       try {
-        let result = new Function('return (' + (daapi.getGlobalFlag({ flag: 'command' })) + ')')()
+        let result = new Function('return (' + (daapi.getGlobalFlag({ flag: 'command_console' })) + ')')()
         if(result) {
           daapi.pushInteractionModalQueue({
             title: 'Command Console',
             message: 'Output: ' + JSON.stringify(result),
-            image: daapi.requireImage('/command/command.svg'),
+            image: daapi.requireImage('/command_console/command.svg'),
             inputs: [
               {
                 type: 'text',
@@ -74,7 +75,7 @@
         daapi.pushInteractionModalQueue({
           title: 'Command Console',
           message: 'Error: ' + err.name + ': ' + err.message,
-          image: daapi.requireImage('/command/command.svg'),
+          image: daapi.requireImage('/command_console/command.svg'),
           inputs: [
             {
               type: 'text',

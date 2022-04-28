eb35769358f383d0574e68236e767fdb7608198c
CitizenOfRome <rome@rangergo.net>
2022-04-29 01:29:52 +0530

    play_scenario mod

---
+++ mods/play_scenario/main.js
@@ -0,0 +1,100 @@
+{
+  canTriggerIfUnavailable: true,
+  checkType: 'general',
+  checkAndAct() {
+    daapi.addGlobalAction({
+      key: 'playScenario',
+      action: {
+        title: 'Play a Scenario',
+        icon: daapi.requireImage('/play_scenario/play.svg'),
+        isAvailable: true,
+        process: {
+          event: '/play_scenario/main',
+          method: 'process'
+        }
+      }
+    })
+  },
+  methods: {
+  	process() {
+  		let scenarios = [
+  			'/play_scenario/scenarios/gaius_julius_caesar'
+  		]
+  		let state = daapi.getState()
+  		let scaleByClassFactor = daapi.calculateScaleByClassFactor()
+  		let options = []
+  		scenarios.forEach((scenario) => {
+  			let scenarioData = daapi.modData.events[scenario]
+  			let property = {}
+  			let combinedProperties = {...state.current.propertyDetails, ...scenarioData.property}
+  			for(var prop in combinedProperties) {
+  				if(!combinedProperties.hasOwnProperty(prop)) {
+  					continue
+  				}
+  				property[prop] = (parseInt(scenarioData.property[prop], 10) || 0) - (parseInt(state.current.propertyDetails[prop], 10) || 0)
+  			}
+  			options.push({
+          text: scenarioData.title,
+          statChanges: {
+            cash: ((parseFloat(scenarioData.cash) || 0) - (parseFloat(state.current.cash) || 0)) / scaleByClassFactor,
+            influence: ((parseFloat(scenarioData.influence) || 0) - (parseFloat(state.current.influence) || 0)) / scaleByClassFactor,
+            property
+          },
+          action:{
+            event: '/play_scenario/main',
+            method: 'playScenario',
+            context: {
+            	scenario
+            }
+          }
+        })
+  		})
+  		daapi.pushInteractionModalQueue({
+        title: 'Play a Scenario',
+        message: 'Play one of these families & scenarios? Your current family & progress will be lost',
+        image: daapi.requireImage('/play_scenario/play.svg'),
+        options: [
+          ...options,
+          {
+            text: 'Not right now'
+          }
+        ]
+      })
+  	},
+  	playScenario({ scenario }) {
+  		let scenarioData = daapi.modData.events[scenario]
+  		daapi.setDate(scenarioData.date)
+  		let genCharIdMapping = {}
+  		// load characters & linked dynasties
+  		for(let tmpCharacterId in scenarioData.characters) {
+  			if(!scenarioData.characters.hasOwnProperty(tmpCharacterId)) {
+  				continue
+  			}
+  			let genCharId = daapi.generateCharacter({ 
+  				characterFeatures: scenarioData.characters[tmpCharacterId], 
+  				dynastyFeatures: scenarioData.dynasties[scenarioData.characters[tmpCharacterId].dynastyId]
+  			})
+  			genCharIdMapping[tmpCharacterId] = genCharId
+  		}
+  		for(let tmpCharacterId in genCharIdMapping) {
+  			let character = scenarioData.characters[tmpCharacterId]
+  			character.id = genCharIdMapping[tmpCharacterId]
+  			character.fatherId = genCharIdMapping[character.fatherId]
+  			character.motherId = genCharIdMapping[character.motherId]
+  			character.spouseId = genCharIdMapping[character.spouseId]
+  			let newChildrenIds = []
+  			character.childrenIds = character.childrenIds || []
+  			character.childrenIds.forEach((childId) => {
+  				newChildrenIds.push(genCharIdMapping[childId])
+  			})
+  			character.childrenIds = newChildrenIds
+  			daapi.updateCharacter({
+  				characterId: genCharIdMapping[tmpCharacterId],
+  				character
+  			})
+  		}
+  		// play as
+  		daapi.setCurrentCharacter({ characterId: genCharIdMapping[scenarioData.characterId] })
+  	}
+  }
+}
---
+++ mods/play_scenario/scenarios/gaius_julius_caesar.js
@@ -0,0 +1,174 @@
+{
+	title: 'Gaius Julius Caesar, Quintilis, 652 A.U.C',
+  characterId: 'Gaius_Julius_Caesar_Father',
+	date: {
+		day: 14,
+		month: 6,
+		year: 661
+	},
+  cash: 250000,
+  influence: 30000,
+  property: {
+    farmland: 25,
+    vinyard: 20,
+    latifundiumAnimal: 2
+  },
+	dynasties: {
+		Julius_Caesar: {
+      id: 'Julius_Caesar',
+      nomen: 'Julius',
+      cognomen: 'Caesar',
+      prestige: 150000,
+      heritage: 'roman_patrician'
+		},
+		Aurelius_Cotta: {
+      id: 'Aurelius_Cotta',
+      nomen: 'Aurelius',
+      cognomen: 'Cotta',
+      prestige: 100000,
+      heritage: 'roman_novus_homo'
+		}
+	},
+	characters: {
+		'Gaius_Julius_Caesar_Father': {
+      id: 'Gaius_Julius_Caesar_Father',
+      dynastyId: 'Julius_Caesar',
+      isAlive: true,
+      isMale: true,
+      praenomen: 'Gaius',
+      agnomen: '',
+      birthMonth: 3,
+      birthYear: 613,
+      job: 'senator',
+      jobLevel: 0,
+      spouseId: 'Aurelia_Mother',
+      fatherId: null,
+      motherId: null,
+      childrenIds: ['Julia_Major', 'Julia_Minor', 'Gaius_Julius_Caesar'],
+      traits: ['oratorDeliberative', 'formerMagistrate', 'formerMilitaryTribune', 'veteran', 'senator', 'formerQuaestor', 'formerPraetor', 'formerProPraetor'],
+      skills: {
+        intelligence: Math.round(Math.random() * 10) + 20,
+        stewardship: Math.round(Math.random() * 10) + 20,
+        eloquence: Math.round(Math.random() * 10) + 20,
+        combat: Math.round(Math.random() * 10) + 20
+      },
+      look: {
+        group: 'roman',
+        type: 'black'
+      },
+      inheritance: 0
+		},
+		'Aurelia_Mother': {
+      id: 'Aurelia_Mother',
+      dynastyId: 'Aurelius_Cotta',
+      isAlive: true,
+      isMale: false,
+      praenomen: 'Aurelia',
+      agnomen: '',
+      birthMonth: 5,
+      birthYear: 633,
+      job: null,
+      jobLevel: 0,
+      spouseId: 'Gaius_Julius_Caesar_Father',
+      fatherId: null,
+      motherId: null,
+      childrenIds: ['Julia_Major', 'Julia_Minor', 'Gaius_Julius_Caesar'],
+      traits: [],
+      skills: {
+        intelligence: Math.round(Math.random() * 10) + 10,
+        stewardship: Math.round(Math.random() * 10) + 10,
+        eloquence: Math.round(Math.random() * 10) + 10,
+        combat: Math.round(Math.random() * 10) + 10
+      },
+      look: {
+        group: 'roman',
+        type: 'brown'
+      },
+      inheritance: 50000
+		},
+		'Julia_Major': {
+      id: 'Julia_Major',
+      dynastyId: 'Julius_Caesar',
+      isAlive: true,
+      isMale: false,
+      praenomen: 'Julia',
+      agnomen: 'Major',
+      birthMonth: 8,
+      birthYear: 651,
+      job: null,
+      jobLevel: 0,
+      spouseId: null,
+      fatherId: 'Gaius_Julius_Caesar_Father',
+      motherId: 'Aurelia_Mother',
+      childrenIds: [],
+      traits: ['literate'],
+      skills: {
+        intelligence: Math.round(Math.random() * 10) + 10,
+        stewardship: Math.round(Math.random() * 10) + 10,
+        eloquence: Math.round(Math.random() * 10) + 10,
+        combat: Math.round(Math.random() * 10) + 10
+      },
+      look: {
+        group: 'roman',
+        type: 'black'
+      },
+      inheritance: 0
+		},
+		'Julia_Minor': {
+      id: 'Julia_Minor',
+      dynastyId: 'Julius_Caesar',
+      isAlive: true,
+      isMale: false,
+      praenomen: 'Julia',
+      agnomen: 'Minor',
+      birthMonth: 2,
+      birthYear: 652,
+      job: null,
+      jobLevel: 0,
+      spouseId: null,
+      fatherId: 'Gaius_Julius_Caesar_Father',
+      motherId: 'Aurelia_Mother',
+      childrenIds: [],
+      traits: ['literate'],
+      skills: {
+        intelligence: Math.round(Math.random() * 10) + 10,
+        stewardship: Math.round(Math.random() * 10) + 10,
+        eloquence: Math.round(Math.random() * 10) + 10,
+        combat: Math.round(Math.random() * 10) + 10
+      },
+      look: {
+        group: 'roman',
+        type: 'brown'
+      },
+      inheritance: 0
+		},
+		'Gaius_Julius_Caesar': {
+      id: 'Gaius_Julius_Caesar',
+      dynastyId: 'Julius_Caesar',
+      isAlive: true,
+      isMale: true,
+      praenomen: 'Gaius',
+      agnomen: '',
+      birthMonth: 6,
+      birthYear: 653,
+      job: null,
+      jobLevel: 0,
+      spouseId: null,
+      fatherId: 'Gaius_Julius_Caesar_Father',
+      motherId: 'Aurelia_Mother',
+      childrenIds: [],
+      traits: ['literate'],
+      skills: {
+        intelligence: Math.round(Math.random() * 10) + 10,
+        stewardship: Math.round(Math.random() * 10) + 10,
+        eloquence: Math.round(Math.random() * 10) + 10,
+        combat: Math.round(Math.random() * 10) + 10
+      },
+      look: {
+        group: 'roman',
+        type: 'black'
+      },
+      inheritance: 0
+		}
+	}
+}

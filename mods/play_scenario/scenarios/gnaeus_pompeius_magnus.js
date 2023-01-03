cc75bf9baff6929c55a3ce14a76fe704fcfb8617
rahdnus <rahdnus119@gmail.com>
2023-01-03 10:03:09 +0530

    removed pompey test mother

--- mods/play_scenario/scenarios/gnaeus_pompeius_magnus.js
+++ mods/play_scenario/scenarios/gnaeus_pompeius_magnus.js
@@ -22,25 +22,25 @@
     chicken: 25
   },
   dynasties: {
-    'ppp': {
-      id: 'ppp',
+    'Pompeius_Strabo': {
+      id: 'Pompeius_Strabo',
       nomen: 'Pompeius',
       cognomen: 'Strabo',
       prestige: 1500,
       heritage: 'roman_plebian'
     },
-    'Mother_Gen': {
-      id: 'Mother_Gen',
-      nomen: 'Mother',
-      cognomen: 'Gen',
-      prestige: 1500,
-      heritage: 'roman_plebian'
-    }
+    // 'Mother_Gen': {
+    //   id: 'Mother_Gen',
+    //   nomen: 'Mother',
+    //   cognomen: 'Gen',
+    //   prestige: 1500,
+    //   heritage: 'roman_plebian'
+    // }
   },
   characters: {
     'Gnaeus_Pompeius_Magnus_Father': {
       id: 'Gnaeus_Pompeius_Magnus_Father',
-      dynastyId: 'ppp',
+      dynastyId: 'Pompeius_Strabo',
       isDead: false,
       gender: 'male',
       isMale: true,
@@ -52,7 +52,7 @@
       // deathYear: 667,
       job: null,
       jobLevel: 0,
-      spouseId: 'Gnaeus_Pompeius_Magnus_Mother',
+      spouseId: null,
       fatherId: null,
       motherId: null,
       childrenIds: ['Gnaeus_Pompeius_Magnus'],
@@ -67,37 +67,37 @@
       flagPlayScenarioModIsPompey: true,
       flagAssignedPersonalityTrait: true
     },
-    'Gnaeus_Pompeius_Magnus_Mother': {
-      id: 'Gnaeus_Pompeius_Magnus_Mother',
-      dynastyId: 'Mother_Gen',
-      // isDead: true,
-      gender: 'female',
-      isMale: false,
-      praenomen: 'Mother',
-      agnomen: '',
-      birthMonth: 11,
-      birthYear: 619,
-      // deathMonth: 3,
-      // deathYear: 667,
-      job: null,
-      jobLevel: 0,
-      spouseId: 'Gnaeus_Pompeius_Magnus_Father',
-      fatherId: null,
-      motherId: null,
-      childrenIds: ['Gnaeus_Pompeius_Magnus'],
-      traits: ['veteran', 'oratorDeliberative'],
-      skills: {
-        intelligence: Math.round(Math.random() * 10) + 10,
-        stewardship: Math.round(Math.random() * 10),
-        eloquence: Math.round(Math.random() * 10),
-        combat: Math.round(Math.random() * 10) + 7
-      },
-      inheritance: 0,
-      flagAssignedPersonalityTrait: true
-    },
+    // 'Gnaeus_Pompeius_Magnus_Mother': {
+    //   id: 'Gnaeus_Pompeius_Magnus_Mother',
+    //   dynastyId: 'Mother_Gen',
+    //   // isDead: true,
+    //   gender: 'female',
+    //   isMale: false,
+    //   praenomen: 'Mother',
+    //   agnomen: '',
+    //   birthMonth: 11,
+    //   birthYear: 619,
+    //   // deathMonth: 3,
+    //   // deathYear: 667,
+    //   job: null,
+    //   jobLevel: 0,
+    //   spouseId: 'Gnaeus_Pompeius_Magnus_Father',
+    //   fatherId: null,
+    //   motherId: null,
+    //   childrenIds: ['Gnaeus_Pompeius_Magnus'],
+    //   traits: ['veteran', 'oratorDeliberative'],
+    //   skills: {
+    //     intelligence: Math.round(Math.random() * 10) + 10,
+    //     stewardship: Math.round(Math.random() * 10),
+    //     eloquence: Math.round(Math.random() * 10),
+    //     combat: Math.round(Math.random() * 10) + 7
+    //   },
+    //   inheritance: 0,
+    //   flagAssignedPersonalityTrait: true
+    // },
     'Gnaeus_Pompeius_Magnus': {
       id: 'Gnaeus_Pompeius_Magnus',
-      dynastyId: 'ppp',
+      dynastyId: 'Pompeius_Strabo',
       isDead: false,
       gender: 'male',
       isMale: true,
@@ -110,7 +110,7 @@
       jobLevel: 0,
       spouseId: null,
       fatherId: 'Gnaeus_Pompeius_Magnus_Father',
-      motherId: 'Gnaeus_Pompeius_Magnus_Mother',
+      motherId: null,
       childrenIds: [],
       traits: ['genius', 'erudite', 'oratorJudicial', 'philosopher', 'honorable', 'formerPlebianTribune', 'formerPraetor'],
       skills: {
@@ -126,169 +126,4 @@
       flagAssignedPersonalityTrait: true,
     }
   }
-  // dynasties: {
-  //   'Vipsanius_Agrippa': {
-  //     id: 'Vipsanius_Agrippa',
-  //     nomen: 'Vipsanius',
-  //     cognomen: 'Agrippa',
-  //     prestige: 1500,
-  //     heritage: 'roman_plebian'
-  //   },
-  //   Aurelius_Mysticus: {
-  //     id: 'Aurelius_Mysticus',
-  //     nomen: 'Aurelius',
-  //     cognomen: 'Mysticus',
-  //     prestige: 100,
-  //     heritage: 'roman_freedman'
-  //   }
-  // },
-  // characters: {
-  //   'Marcus_Vipsanius_Agrippa_Father': {
-  //     id: 'Marcus_Vipsanius_Agrippa_Father',
-  //     dynastyId: 'Vipsanius_Agrippa',
-  //     isDead: true,
-  //     gender: 'male',
-  //     isMale: true,
-  //     praenomen: 'Lucius',
-  //     agnomen: '',
-  //     birthMonth: 11,
-  //     birthYear: 664,
-  //     deathMonth: 3,
-  //     deathYear: 705,
-  //     job: null,
-  //     jobLevel: 0,
-  //     spouseId: 'Aurelia_Mother',
-  //     fatherId: null,
-  //     motherId: null,
-  //     childrenIds: ['Lucius_Vipsanius', 'Vipsania_Polla', 'Marcus_Vipsanius_Agrippa'],
-  //     traits: ['veteran', 'oratorDeliberative'],
-  //     skills: {
-  //       intelligence: Math.round(Math.random() * 10) + 10,
-  //       stewardship: Math.round(Math.random() * 10),
-  //       eloquence: Math.round(Math.random() * 10),
-  //       combat: Math.round(Math.random() * 10) + 7
-  //     },
-  //     inheritance: 0,
-  //     flagAssignedPersonalityTrait: true
-  //   },
-  //   'Aurelia_Mother': {
-  //     id: 'Aurelia_Mother',
-  //     dynastyId: 'Aurelius_Mysticus',
-  //     isDead: false,
-  //     gender: 'female',
-  //     isMale: false,
-  //     praenomen: 'Aurelia',
-  //     agnomen: '',
-  //     birthMonth: 9,
-  //     birthYear: 669,
-  //     job: 'physician',
-  //     jobLevel: 6,
-  //     spouseId: 'Marcus_Vipsanius_Agrippa_Father',
-  //     fatherId: null,
-  //     motherId: null,
-  //     childrenIds: ['Lucius_Vipsanius', 'Vipsania_Polla', 'Marcus_Vipsanius_Agrippa'],
-  //     traits: ['educated', 'illness', 'mystic'],
-  //     skills: {
-  //       intelligence: Math.round(Math.random() * 10) + 10,
-  //       stewardship: Math.round(Math.random() * 10) + 10,
-  //       eloquence: Math.round(Math.random() * 10) + 10,
-  //       combat: Math.round(Math.random() * 10) + 10
-  //     },
-  //     look: {
-  //       group: 'roman',
-  //       type: 'auburn'
-  //     },
-  //     inheritance: 0,
-  //     flagAssignedPersonalityTrait: true
-  //   },
-  //   'Marcus_Vipsanius_Agrippa': {
-  //     id: 'Marcus_Vipsanius_Agrippa',
-  //     dynastyId: 'Vipsanius_Agrippa',
-  //     isDead: false,
-  //     gender: 'male',
-  //     isMale: true,
-  //     praenomen: 'Marcus',
-  //     agnomen: '',
-  //     cognomen: 'Agrippa',
-  //     birthMonth: 1,
-  //     birthYear: 690,
-  //     job: null,
-  //     jobLevel: 0,
-  //     spouseId: null,
-  //     fatherId: 'Marcus_Vipsanius_Agrippa_Father',
-  //     motherId: 'Aurelia_Mother',
-  //     childrenIds: [],
-  //     traits: ['genius', 'erudite', 'oratorJudicial', 'philosopher', 'honorable', 'formerPlebianTribune', 'formerPraetor'],
-  //     skills: {
-  //       intelligence: Math.round(Math.random() * 10) + 20,
-  //       stewardship: Math.round(Math.random() * 10) + 10,
-  //       eloquence: Math.round(Math.random() * 10) + 15,
-  //       combat: Math.round(Math.random() * 10) + 10
-  //     },
-  //     look: {
-  //       group: 'roman',
-  //       type: 'black'
-  //     },
-  //     flagAssignedPersonalityTrait: true,
-  //     flagPlayScenarioModIsPompey: true
-  //   },
-  //   'Vipsania_Polla': {
-  //     id: 'Vipsania_Polla',
-  //     dynastyId: 'Vipsanius_Agrippa',
-  //     isDead: false,
-  //     gender: 'female',
-  //     isMale: false,
-  //     praenomen: 'Vipsania Polla',
-  //     nomen: ' ',
-  //     agnomen: '',
-  //     cognomen: ' ',
-  //     birthMonth: 2,
-  //     birthYear: 684,
-  //     job: null,
-  //     jobLevel: 0,
-  //     fatherId: 'Marcus_Vipsanius_Agrippa_Father',
-  //     motherId: 'Aurelia_Mother',
-  //     childrenIds: [],
-  //     traits: ['oratorDeliberative', 'honorable'],
-  //     skills: {
-  //       intelligence: Math.round(Math.random() * 10) + 10,
-  //       stewardship: Math.round(Math.random() * 10) + 5,
-  //       eloquence: Math.round(Math.random() * 10) + 10,
-  //       combat: Math.round(Math.random() * 10) + 5
-  //     },
-  //     look: {
-  //       group: 'roman',
-  //       type: 'black'
-  //     },
-  //     flagAssignedPersonalityTrait: true
-  //   },
-  //   'Lucius_Vipsanius': {
-  //     id: 'Lucius_Vipsanius',
-  //     dynastyId: 'Vipsanius_Agrippa',
-  //     isDead: false,
-  //     gender: 'male',
-  //     isMale: true,
-  //     praenomen: 'Lucius',
-  //     agnomen: '',
-  //     birthMonth: 3,
-  //     birthYear: 685,
-  //     job: null,
-  //     jobLevel: 0,
-  //     fatherId: 'Marcus_Vipsanius_Agrippa_Father',
-  //     motherId: 'Aurelia_Mother',
-  //     childrenIds: [],
-  //     traits: ['oratorJudicial'],
-  //     skills: {
-  //       intelligence: Math.round(Math.random() * 10) + 10,
-  //       stewardship: Math.round(Math.random() * 10),
-  //       eloquence: Math.round(Math.random() * 10),
-  //       combat: Math.round(Math.random() * 10) + 10
-  //     },
-  //     look: {
-  //       group: 'roman',
-  //       type: 'black'
-  //     },
-  //     flagAssignedPersonalityTrait: false
-  //   }
-  // }
 }

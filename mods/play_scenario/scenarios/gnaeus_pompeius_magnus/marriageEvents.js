cd8522580bc9ca3e026df962a58050e80af1540f
rahdnus <rahdnus119@gmail.com>
2023-01-05 21:15:08 +0530

    added unique stat changes and message for marriage

--- mods/play_scenario/scenarios/gnaeus_pompeius_magnus/marriageEvents.js
+++ mods/play_scenario/scenarios/gnaeus_pompeius_magnus/marriageEvents.js
@@ -26,68 +26,104 @@
             let potentialSpouse = daapi.getCharacter({ characterId: potentialSpouseID })
 
             let marriageMessage = ''
+            let marriageAcceptStatChanges = {}
+            let marriageRejectStatChanges = {}
+            let divorceAcceptStatChanges = {}
+            let divorceRejectStatChanges = {}
             switch (spouseIndex) {
               case 0:
                 marriageMessage = 'Your Father has been accused of stealing public prperty.As your father\'s heir, you are being put on trial. Through thorough invesitgation you discovered that the theft was committed by one of your father\'s freedmen. You present your defense to the judge and directly confront the accuser. As a result, You have succesfully defended your father\'s honour and won the trail.'
-                  +' The judge,Publius Antistius, took a liking to you and offered his daughter ,'
+                  + ' The judge,Publius Antistius, took a liking to you and offered his daughter ,'
                   + `[c|${potentialSpouse.id}|${"Antistia"}]` + '\'s  hand in marriage'
                   + '\n What will you do?'
+                marriageAcceptStatChanges = {
+                  prestige: +1000 / scalingFactor,
+                  influence: +2000 / scalingFactor,
+                  property: {
+                    insulae: +1,
+                    horse: +1
+                  }
+                }
+                marriageRejectStatChanges = {
+                  prestige: -3000 / scalingFactor,
+                  influence: -1000 / scalingFactor,
+                }
+                divorceAcceptStatChanges = {
+                  prestige: +1000 / scalingFactor,
+                  influence: -500 / scalingFactor,
+                }
+                divorceRejectStatChanges = {
+                  prestige: -500 / scalingFactor,
+                  influence: +2000 / scalingFactor,
+                }
                 break;
               case 1:
-                marriageMessage = 'Dictator Sulla admires your administration skill and wishes to strengthen ties with you. He and his wife, Metella, wish that '+ `[c|${characterId}|${"you"}].`+' to marry their step-daughter,  '
+                marriageMessage = 'Dictator Sulla admires your administration skill and wishes to strengthen ties with you. He and his wife, Metella, wish that ' + `[c|${characterId}|${"you"}]` + ' marry their step-daughter,  '
                   + `[c|${potentialSpouse.id}|${"Aemilia"}].`
-                  +'This could be a great oppurtunity to gain Sulla\'s favor.'
+                  + 'This could be a great oppurtunity to gain Sulla\'s favor.'
                   + '\n What will you do?'
+                marriageAcceptStatChanges = {
+                  prestige: +5000 / scalingFactor,
+                  influence: +3000 / scalingFactor,
+                  property: {
+                    seafaringTradeships: +3,
+                  }
+                }
+                marriageRejectStatChanges = {
+                  prestige: -7000 / scalingFactor,
+                  influence: -1000 / scalingFactor,
+                }
+                divorceAcceptStatChanges = {
+                  prestige: +4000 / scalingFactor,
+                  influence: -1500 / scalingFactor,
+                }
+                divorceRejectStatChanges = {
+                  prestige: -5000 / scalingFactor,
+                  influence: +1000 / scalingFactor,
+                }
                 break;
               case 2:
                 marriageMessage = 'Julius Caesar come to you with a proposal. As fellow members of the First Triumvirate he wishes to deepen your partnership. He offers his daughter '
                   + `[c|${potentialSpouse.id}|${"Julia"}]` + '\'s  hand in marriage to you,' + `[c|${characterId}|${"Gnaeus Pompeius Magnus"}].`
-                  +'This could be a great oppurtunity to gain Caesar\'s favor.'
+                  + 'This could be a great oppurtunity to gain Caesar\'s favor and strengthen your position in the Triumvirate.'
                   + '\n What will you do?'
+                marriageAcceptStatChanges = {
+                  prestige: +1000 / scalingFactor,
+                  influence: +2000 / scalingFactor,
+                  property: {
+                    latifundiumFood: +3
+                  }
+                }
+                marriageRejectStatChanges = {
+                  prestige: -3000 / scalingFactor,
+                  influence: -1000 / scalingFactor,
+                }
+                divorceAcceptStatChanges = {
+                  prestige: +10000 / scalingFactor,
+                  influence: -5000 / scalingFactor,
+                }
+                divorceRejectStatChanges = {
+                  prestige: -20000 / scalingFactor,
+                  influence: +10000 / scalingFactor,
+                }
                 break;
 
             }
 
             if (potentialSpouse) {
 
-              let acceptOption =
-              {
-                text: 'I accept.',
-                tooltip: 'This will be a feast to remember.',
-                statChanges: {
-                  cash: -6000 / scalingFactor,
-                  prestige: +2000 / scalingFactor,
-                  influence: +4000 / scalingFactor,
-                  property: {
-                    insulae: +1,
-                    horse: +1
-                  }
-                },
-                disabled: false,
-                action: {
-                  event: '/play_scenario/scenarios/gnaeus_pompeius_magnus/marriageEvents',
-                  method: 'wedding',
-                  context: { characterId: characterId, spouseId: potentialSpouse.id, isMatrilineal: false }
-                }
-              }
-              console.log(character.spouseId)
+              let marriageOptions = []
+
+              // console.log(character.spouseId)
               if (character.spouseId !== false &&
-                (state.characters[character.spouseId] &&
-                  !state.characters[character.spouseId].isDead)) {
+                (state.characters[character.spouseId] && !state.characters[character.spouseId].isDead)) {
+
                 let presentSpouse = daapi.getCharacter({ characterId: character.spouseId })
 
-                acceptOption = {
-                  text: 'Divorce ' + `[c|${character.spouseId}|${presentSpouse.praenomen}]` + ',Marry ' + `[c|${potentialSpouse.id}|${potentialSpouse.praenomen}]`,
+                let acceptDivorceOption = {
+                  text: 'Divorce ' + `[c|${character.spouseId}|${presentSpouse.praenomen}]` + ', Marry ' + `[c|${potentialSpouse.id}|${potentialSpouse.praenomen}]`,
                   tooltip: 'Tis most unfortunate.',
-                  statChanges: {
-                    cash: -6000 / scalingFactor,
-                    prestige: +2000 / scalingFactor,
-                    influence: +4000 / scalingFactor,
-                    property: {
-                      insulae: +1,
-                      horse: +1
-                    }
-                  },
+                  statChanges: divorceAcceptStatChanges,
                   disabled: false,
                   action: {
                     event: '/play_scenario/scenarios/gnaeus_pompeius_magnus/marriageEvents',
@@ -95,25 +131,39 @@
                     context: { characterId: characterId, spouseId: potentialSpouse.id, isMatrilineal: false }
                   }
                 }
+                let rejectDivorceOption =
+                {
+                  text: 'I won\'t abandon my Love.',
+                  tooltip: 'I would never do such a thing',
+                  statChanges: divorceRejectStatChanges
+                }
+                marriageOptions = [acceptDivorceOption, rejectDivorceOption]
               }
-
+              else {
+                let acceptMarriageOption =
+                {
+                  text: 'I accept.',
+                  statChanges: marriageAcceptStatChanges,
+                  disabled: false,
+                  action: {
+                    event: '/play_scenario/scenarios/gnaeus_pompeius_magnus/marriageEvents',
+                    method: 'wedding',
+                    context: { characterId: characterId, spouseId: potentialSpouse.id, isMatrilineal: false }
+                  }
+                }
+                let rejectMarriageOption =
+                {
+                  text: 'I won\'t marry her.',
+                  tooltip: 'Why should I?',
+                  statChanges: marriageRejectStatChanges
+                }
+                marriageOptions = [acceptMarriageOption, rejectMarriageOption]
+              }
               daapi.pushInteractionModalQueue({
                 title: 'Marriage with ' + `${potentialSpouse.praenomen}`,
                 image: daapi.requireImage("/play_scenario/scenarios/gnaeus_pompeius_magnus/ruby_ring_optimized.svg"),
                 message: marriageMessage,
-                options:
-                  [
-                    acceptOption,
-                    {
-                      text: 'I won\'t marry her.',
-                      tooltip: 'Why should I?',
-                      statChanges:
-                      {
-                        prestige: -500 / scalingFactor,
-                        influence: -2000 / scalingFactor
-                      }
-                    }
-                  ]
+                options: marriageOptions
               })
               daapi.setCharacterFlag({ characterId: characterId, flag: `pompey_engagement_${spouseIndex + 1}_over`, data: true })
             }
@@ -125,6 +175,10 @@
   methods: {
     wedding({ characterId, spouseId, isMatrilineal }) {
       daapi.performMarriage({ characterId, spouseId, isMatrilineal })
-    }
+    },
+    // divorceAndMarry({ characterId, presentSpouseId, newSpouseId }) {
+
+    //   daapi.performMarriage({ characterId, newSpouseId, isMatrilineal })
+    // }
   }
 }

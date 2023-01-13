670139259064c9ed4f613f1d2e481738cc3b88e7
CitizenOfRome <rome@rangergo.net>
2023-01-13 19:02:10 +0530

    character info mod

---
+++ mods/character_info/main.js
@@ -0,0 +1,46 @@
+// Opens a command console on the Interaction Modal
+{
+  canTriggerIfUnavailable: true,
+  checkType: 'allCharacters',
+  checkAndAct(characterId) {
+    daapi.addCharacterAction({
+      characterId,
+      key: 'character_info',
+      action: {
+        title: 'Show character info',
+        icon: daapi.requireImage('/character_info/info.svg'),
+        isAvailable: true,
+        hideWhenBusy: false,
+        process: {
+          event: '/character_info/main',
+          method: 'process',
+          context: {
+            characterId
+          }
+        }
+      }
+    })
+  },
+  methods: {
+    process({ characterId }) {
+      let character = daapi.getCharacter({ characterId })
+      daapi.pushInteractionModalQueue({
+        title: 'Character Info',
+        message: 'Select and copy character info below:',
+        image: daapi.requireImage('/character_info/info.svg'),
+        inputs: [
+          {
+            type: 'text',
+            title: 'Character ID',
+            value: JSON.stringify(character.id)
+          },
+          {
+            type: 'textarea',
+            title: 'Character Info',
+            value: JSON.stringify(character)
+          }
+        ]
+      })
+    }
+  }
+}
---
+++ mods/character_info/monthly.js
@@ -0,0 +1,1 @@
+['/character_info/main']
